46a6121061bbc048be59db161cfb1a747499dd73
bengillies <bengillies@gmail.com>
2010-04-22 13:38:30 +0100

    commented out the advanced post tests for the moment as Im not sure we need advanced posting. Also fixed some bugs in the remaining tests

--- test/test_post.py
+++ test/test_post.py
@@ -42,68 +42,69 @@ def test_post_new():
 def test_post_existing():
   #setup
   setup(store)
-  tid = Tiddler("i exist","ben")
+  tid = Tiddler("i exist","jon_private")
   tid.fields['geo.lat'] = u"20"
   tid.fields['geo.long'] = u"2"
   tid.text = "data"
   tid.tags= ["a","b"]
   store.put(tid)
   #run
-  email = mailer.handle_email({"to":"post@ben.tiddlyspace.com","from":"jdlrobson@gmail.com","subject":"i exist","body":"wipeout"})
+  email = mailer.handle_email({"to":"post@jon.tiddlyspace.com","from":"jdlrobson@gmail.com","subject":"i exist","body":"wipeout"})
 
   #verify  
   tid = store.get(Tiddler("i exist","jon_private"))
   assert tid.text == "wipeout"
-  assert tid.fields['geo.lat'] == u"20"
+  assert tid.fields.get('geo.lat') == None
   
-  
-def test_post_advanced(): #the body is how tiddlers are stored in textual representation
-  #setup
-  setup(store)
-  
-  #run
-  advancedBody = '''
-  tags: foo bar [[jon tag with spaces]]
-  foo: val
-  
-  my text
-  '''
-  email = mailer.handle_email({"to":"post@jon.tiddlyspace.com","from":"jdlrobson@gmail.com","subject":"test","body":advancedBody})
-  
-  #verify  
-  try:
-    tid = store.get(Tiddler("test","jon_private"))
-  except NoTiddlerError:
-    assert False == True
-  assert tid.text == ',my text'
-  assert tid.tags == ["foo","bar","jon tag with spaces"]
 
-
-def test_post_advanced_existing(): #the body is how tiddlers are stored in textual representation
-  #setup
-  setup(store)
-  tid = Tiddler("test","jon_private")
-  tid.tag = ["foo"]
-  tid.text = "hello world"
-  tid.fields['foo'] = 'bar'
-  tid.fields['bar'] = 'baz'
-  store.put(tid)
-  
-  advancedBody = '''
-  tags: foo bar [[jon tag with spaces]]
-  foo: val
-
-  my text
-  '''
-  #run
-  email = mailer.handle_email({"to":"post@jon.tiddlyspace.com","from":"jdlrobson@gmail.com","subject":"test","body":advancedBody})
-
-  #verify  
-  try:
-    tid = store.get(Tiddler("test","jon_private"))
-  except NoTiddlerError:
-    assert False == True
-  assert tid.text == ',my text'
-  assert tid.tags == ["foo","bar","jon tag with spaces"]
-  assert tid.fields['foo'] == 'val'
-  assert "bar" not in tiddler.fields
+#commented out until further notice  -- I'm not sure we want/need this?'
+#def test_post_advanced(): #the body is how tiddlers are stored in textual representation
+#  #setup
+#  setup(store)
+#  
+#  #run
+#  advancedBody = '''
+#  tags: foo bar [[jon tag with spaces]]
+#  foo: val
+#  
+#  my text
+#  '''
+#  email = mailer.handle_email({"to":"post@jon.tiddlyspace.com","from":"jdlrobson@gmail.com","subject":"test","body":advancedBody})
+#  
+#  #verify  
+#  try:
+#    tid = store.get(Tiddler("test","jon_private"))
+#  except NoTiddlerError:
+#    assert False == True
+#  assert tid.text == ',my text'
+#  assert tid.tags == ["foo","bar","jon tag with spaces"]
+#
+#
+#def test_post_advanced_existing(): #the body is how tiddlers are stored in textual representation
+#  #setup
+#  setup(store)
+#  tid = Tiddler("test","jon_private")
+#  tid.tag = ["foo"]
+#  tid.text = "hello world"
+#  tid.fields['foo'] = 'bar'
+#  tid.fields['bar'] = 'baz'
+#  store.put(tid)
+#  
+#  advancedBody = '''
+#  tags: foo bar [[jon tag with spaces]]
+#  foo: val
+#
+#  my text
+#  '''
+#  #run
+#  email = mailer.handle_email({"to":"post@jon.tiddlyspace.com","from":"jdlrobson@gmail.com","subject":"test","body":advancedBody})
+#
+#  #verify  
+#  try:
+#    tid = store.get(Tiddler("test","jon_private"))
+#  except NoTiddlerError:
+#    assert False == True
+#  assert tid.text == ',my text'
+#  assert tid.tags == ["foo","bar","jon tag with spaces"]
+#  assert tid.fields['foo'] == 'val'
+#  assert "bar" not in tiddler.fields
